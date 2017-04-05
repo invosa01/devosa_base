20af3b19de923a89a0f81958d20344c55e5cc930
ade <ade@invosa.com>
2017-04-05 11:50:12 +0700

    Update date format to dd-mm-yyyy in date_seting and review for excel report, Testing On Refs #555 @30m

---
+++ db/20170104_add_coloumn_get_jshk_and_pension_no.sql
@@ -0,0 +1,12 @@
+﻿-- Column: get_jshk
+
+-- ALTER TABLE public.hrd_employee DROP COLUMN get_jshk;
+
+ALTER TABLE public.hrd_employee ADD COLUMN get_jshk smallint;
+ALTER TABLE public.hrd_employee ALTER COLUMN get_jshk SET DEFAULT 1;
+
+-- Column: pension_no
+
+-- ALTER TABLE public.hrd_employee DROP COLUMN pension_no;
+
+ALTER TABLE public.hrd_employee ADD COLUMN pension_no character varying(100);
