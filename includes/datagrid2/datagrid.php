3f12e1352c372f4511791d2c641343d56c561980
prasetyo <prasetyo@invosa.com>
2017-05-15 13:52:07 +0700

    Fix datagrid fail to export.

--- includes/datagrid2/datagrid.php
+++ includes/datagrid2/datagrid.php
@@ -683,7 +683,7 @@ class cDataGrid
 
     function _initWorkBook()
     {
-        $this->wkb =&  workbook("-");
+        $this->wkb =& new workbook("-");
         //create custom color index 50
         $this->wkb->set_custom_color(50, 200, 200, 200);
         // Add a worksheet to the file, returning an object to add data to
@@ -2550,7 +2550,7 @@ class cDataGrid
         }
         //echo $strSQL;
         return $arrData;
-        debug($arrData, true);
+        //debug($arrData, true);
     }
 
     function getDataTypeGroup($dataType = null)
