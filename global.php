a0908d66274c1651a8b19751feceb58a8f8ca5f7
faisal <setyawan@invosa.com>
2017-06-13 12:30:30 +0700

    Update Global Privilages

--- global.php
+++ global.php
@@ -39,11 +39,7 @@ function getDataPrivilegesFromDatabase($idGroup)
           ORDER BY f.sequence_no_module, c.menu_level, f.sequence_no_menu";
             $res = $db->execute($strSQL);
             while ($rowDb = $db->fetchrow($res, "ASSOC")) {
-                $menuPrivileges[] = $rowDb;
-                if (array_key_exists('php_file', $rowDb) === true) {
-                    $key = $rowDb['php_file'];
-                    $menuPrivileges[$key] = $rowDb;
-                }
+                $menuPrivileges[$rowDb] = $rowDb;
             }
         }
     }
